cb58fa4c44cc318d9039429d5492b3075614a743
OlexandrOnokalo <Olexandr.onokalo@gmail.com>
2025-09-26 17:56:44 +0300

    salo

---
+++ 05_ADO.NET_FinalWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.NetworkInformation;
+
+namespace _05_ADO.NET_FinalWork
+{
+    internal class Program
+    {
+        class Country
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+
+        class Genre
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public ICollection<Book> Books { get; set; }
+
+        }
+
+        class Publisher
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Country Country{ get; set; }
+        }
+        class Author
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string LastName { get; set; }
+        }
+    
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
